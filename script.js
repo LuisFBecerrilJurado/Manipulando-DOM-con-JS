7639029a4b728cf24be0e56c2e5fafad11cb6ad5
Fran <l.francisco.bj@gmail.com>
2022-10-17 18:53:08 -0500

    Añadiendo funcion de crear divisiones con las tareas

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ const input = document.querySelector("[data-form-input]");
 const createTask = (event) => {
     event.preventDefault();
 
-    const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const input     = document.querySelector("[data-form-input]");
+    const task      = document.querySelector("[ data-task]");
+    const value     = input.value;
+    input.value = '';
+    const content   = `
+                       <div>
+                       <i class="far fa-check-square icon"></i>
+                       <span class="task">${value}</span>
+                       </div>
+                        <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML  = content;
+    console.log(content);
 }
 
 btn.addEventListener("click", createTask);
