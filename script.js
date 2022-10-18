873e63ec8143465e7b55687bbd73eb5fd7ec17ff
Fran <l.francisco.bj@gmail.com>
2022-10-17 19:59:11 -0500

    Ajustando archivo script para creación de listas dinamicas y marcado de completo o incompleto

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 const btn = document.querySelector("[data-form-btn]");
 const input = document.querySelector("[data-form-input]");
 
-const createTask = (event) => {
+( () =>{
+    const createTask = (event) => {
     event.preventDefault();
-
-    const input     = document.querySelector("[data-form-input]");
-    const task      = document.querySelector("[ data-task]");
-    const value     = input.value;
-    input.value = '';
-    const content   = `
-                       <div>
-                       <i class="far fa-check-square icon"></i>
-                       <span class="task">${value}</span>
-                       </div>
-                        <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML  = content;
-    console.log(content);
+    const input         = document.querySelector("[data-form-input]");
+    const value         = input.value;
+    const task          = document.createElement("li");
+    const list          = document.querySelector("[data-list]");
+    const taskContent   = document.createElement('div');
+    const title         = document.createElement('span');
+    input.value         = '';
+    task.classList.add("card");
+    title.classList.add("task");
+    title.innerText     = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(title);
+    const content       = `
+                       <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.appendChild(taskContent);
+    list.appendChild(task);
 }
 
 btn.addEventListener("click", createTask);
 
+const checkComplete = () =>{
+    const i = document.createElement('i');
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click",completeTask);
+    return i;
+}
+
+const completeTask= (event) =>{
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+}
+}) ()
